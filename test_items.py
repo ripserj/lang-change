54db3a1481b3c658fa12aa1811c03be616b1f00a
ripserj <all4tv@yandex.ru>
2021-09-08 16:26:42 +0300

    VSE GOTOVO

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 def test_item_language(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(2)
+    time.sleep(30)
     btn = browser.find_element_by_class_name("btn-add-to-basket")
     assert btn.is_enabled(), "NO BUTTON!"
 
